6d3b420c5ffd943937d0df985677d66352541245
avanawesome <144808036+avanawesome@users.noreply.github.com>
2024-09-16 20:54:19 -0300

    Refactor webpack configuration and remove unused files

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-78c92fac7aa8fdd8.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-0d931c84c330c851.js","static/chunks/fd9d1056-8042ad09b3c311e2.js","static/chunks/23-9946c8ebee5ba582.js","static/chunks/main-app-97f3d3839bf0e1e2.js"],pages:{"/_app":["static/chunks/webpack-0d931c84c330c851.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-80e01d7209683a37.js","static/chunks/pages/_app-6a626577ffa902a4.js"],"/_error":["static/chunks/webpack-0d931c84c330c851.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-80e01d7209683a37.js","static/chunks/pages/_error-1be831200e60c5c0.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
